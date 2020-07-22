afb4294eb0b44de56cba6e6837e711ebcd25d7ae
Santiago8888 <santiago.marti67@gmail.com>
2020-07-22 00:09:50 -0500

    Initial commit, output CSV.

---
+++ quiz/csv.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	// Open the file
+	csvfile, err := os.Open("quiz.csv")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+
+	// Parse the file
+	r := csv.NewReader(csvfile)
+
+	// Iterate through the records
+	for {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Question: %s Answer %s\n", record[0], record[1])
+	}
+}
