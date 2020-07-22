1ac8d32685201bdd11c520ca2126568094724d07
Santiago8888 <santiago.marti67@gmail.com>
2020-07-22 01:12:39 -0500

    Integrate timeout into quiz.

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -8,9 +8,28 @@ import (
 	"os"
 	"bufio"
 	"strings"
+	"time"
 )
 
 func main() {
+    c1 := make(chan string, 1)
+
+    // Run your long running function in it's own goroutine and pass back it's
+    // response into our channel.
+    go func() {
+        quiz()
+    }()
+
+    // Listen on our channel AND a timeout channel - which ever happens first.
+    select {
+    case res := <-c1:
+        fmt.Println(res)
+    case <-time.After(3 * time.Second):
+        fmt.Println("\nout of time :(")
+    }
+}
+
+func quiz(){
 	// Open the file
 	csvfile, err := os.Open("quiz.csv")
 	if err != nil {
