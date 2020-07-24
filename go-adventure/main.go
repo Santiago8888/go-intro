9894afbb69f17d1d1627cc2d4944c29420aa7711
Santiago8888 <santiago.marti67@gmail.com>
2020-07-24 02:19:40 -0500

    Output options, next arc.

--- go-adventure/main.go
+++ go-adventure/main.go
@@ -35,14 +35,25 @@ func main() {
 	v := intro.(map[string]interface{})
 	fmt.Printf("TITLE: %+v\n", v["title"])
 
+
 	story := v["story"]
 	v1 := story.([]interface{})
-	// fmt.Println(v1)
 
 	for i, k := range v1 {
 		if i == 0 {
 			fmt.Printf("STORY: %+v\n", k)
 		}
+	}
+
+
+	options := v["options"]
+	v2 := options.([]interface{})
+
+	fmt.Println("OPTIONS:")
+	for i, k := range v2 {
+		option := k.(map[string]interface{})
+		arc := option["arc"]
+		fmt.Printf("%d. %+v\n", i + 1, arc)
 	}
 
 
