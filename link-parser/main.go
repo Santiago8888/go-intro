27541c49e05b8212931f619bde0c9c8549213b72
Santiago8888 <santiago.marti67@gmail.com>
2020-07-31 01:42:45 -0500

    Read HTML, filter links and print attrs & text.

---
+++ link-parser/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"os"
+	"log"
+	"fmt"
+	"golang.org/x/net/html"
+)
+
+
+func main() {
+	r, err := os.Open("ex1.html")
+
+	if err != nil {
+		log.Fatalln("Couldn't open the html file", err)
+	}
+
+	t := ""
+	z := html.NewTokenizer(r)
+
+	for {
+		tt := z.Next()
+		if tt == html.ErrorToken {
+			return
+		}
+
+		if tt == html.TextToken && t == "a" {
+			fmt.Println(string(z.Text()))
+		}
+
+		name, _ := z.TagName()
+		if "a" == string(name) && tt == html.StartTagToken {
+			_, attr, _ := z.TagAttr()
+			
+			fmt.Println(string(attr))
+			t = "a"
+		} else { t = "" }
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
