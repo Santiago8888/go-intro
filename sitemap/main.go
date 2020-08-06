cd1ee20cf4c7300163539d6010536eb54e31b5e6
Santiago8888 <santiago.marti67@gmail.com>
2020-08-05 20:12:49 -0500

    Filter out non-cross domain links.

--- sitemap/main.go
+++ sitemap/main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
 	"./parser"
 )
 
 func main() {
-	links := parser.Parse("https://www.calhoun.io/")
-	// fmt.Println(links[0].Text)
-	// fmt.Println(links[0].Href)
+	url := "https://www.calhoun.io/"
+	links := parser.Parse(url)
+	// fmt.Println(links)
+	
+	domainLinks := make([]parser.Link, 0)
+	for _, link := range links {
+		// fmt.Println(link)
+		if strings.HasPrefix(string(link.Href[0]), "/"){
+			// fmt.Println(link.Href)
+			domainLinks = append(domainLinks, link)
+		} else if strings.HasPrefix(string(link.Href), url){
+			// fmt.Println(link.Href)
+			domainLinks = append(domainLinks, link)
+		}
+	}
 
-	fmt.Println(links)
+	fmt.Println(domainLinks)
 }
 
--- sitemap/sitemap.md
+++ sitemap/sitemap.md
@@ -8,7 +8,7 @@ Iteratevily retrieve a website's links to build a sitemap on XML protocol.
 - [X] Given a URL fetch the HTML Document.
 - [X] Convert the link-parser into a package.
 - [X] Consume a medium size website and fetch links.
-- [ ] Filter out cross-domain links.
+- [X] Filter out cross-domain links.
 - [ ] Build a set of same-domain links.
 - [ ] Iterate through non-visited links from the set's links.
 - [ ] Convert the link's set into a valid XML document.
