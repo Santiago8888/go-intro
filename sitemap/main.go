4793c8a7cd04230f4810b754b8a58f411cf7e73b
Santiago8888 <santiago.marti67@gmail.com>
2020-08-05 20:52:20 -0500

    Filter out repited links function.

--- sitemap/main.go
+++ sitemap/main.go
@@ -8,7 +8,36 @@ import (
 )
 
 func main() {
-	url := "https://www.calhoun.io/"
+	url := "https://www.calhoun.io"
+	links := fetchLinks(url)
+	fmt.Println(links)
+}
+
+func getUniqueLinks(links[]parser.Link) []parser.Link{
+	// append([]int{1,2}, []int{3,4}...)
+	uniqueLinks := make([]parser.Link, 0)
+
+	for _, link := range links {
+		// fmt.Println(link.Text)
+		isUnique := true
+		for _, uniqueLink := range uniqueLinks {
+			// fmt.Println(uniqueLink.Text)
+			if link.Href == uniqueLink.Href {
+				isUnique = false
+				break
+			}
+		}
+
+		if isUnique == true {
+			uniqueLinks = append(uniqueLinks, link)
+		}
+
+	}
+
+	return uniqueLinks
+}
+
+func fetchLinks(url string) []parser.Link {
 	links := parser.Parse(url)
 	// fmt.Println(links)
 	
@@ -17,6 +46,7 @@ func main() {
 		// fmt.Println(link)
 		if strings.HasPrefix(string(link.Href[0]), "/"){
 			// fmt.Println(link.Href)
+			link.Href = url + link.Href
 			domainLinks = append(domainLinks, link)
 		} else if strings.HasPrefix(string(link.Href), url){
 			// fmt.Println(link.Href)
@@ -24,6 +54,7 @@ func main() {
 		}
 	}
 
-	fmt.Println(domainLinks)
+	uniqueLinks := getUniqueLinks(domainLinks)
+	return uniqueLinks
 }
 
